0570cc6138d52b8934b97eb16c4e7500a7237d31
Sean Liang <13289352+seanliang10@users.noreply.github.com>
2025-09-26 21:20:34 +0930

    update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.museframe.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 30010
+        versionCode = 300100
         versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
